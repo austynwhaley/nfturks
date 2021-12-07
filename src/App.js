634f1bc7881d68df0d53a566c38aef02de933ba1
Austyn Whaley <austyn_whaley@yahoo.com>
2021-12-07 15:01:17 -0500

    hasrrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, {useState} from 'react';
 import Navbar from './Navbar/Navbar';
 import { Button } from './Button/Button'
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 function App() {
   const [walletAddress, setWalletAddress] = useState('')
   const [showMessage, setMessage] = useState(false)
@@ -12,7 +12,7 @@ function App() {
   }
   
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
         <Navbar walletAddress={walletAddress} setWalletAddress={setWalletAddress}/>
         <header className="App-header">
@@ -23,7 +23,7 @@ function App() {
           <iframe title="Turk" height='500' width='500'  frameborder="0"  src="https://sketchfab.com/models/02f3b844a948482d80518e3803d79a2b/embed?autospin=1&autostart=1"> </iframe>
         </header>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
